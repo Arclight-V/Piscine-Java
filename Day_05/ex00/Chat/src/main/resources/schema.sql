6a6831896a0efb9ace899e5cb6775f642ca07310
Arclight-V <vladimtor@gmail.com>
2021-07-05 19:08:21 +0300

    fix data.sql

--- Day_05/ex00/Chat/src/main/resources/schema.sql
+++ Day_05/ex00/Chat/src/main/resources/schema.sql
@@ -1,22 +1,28 @@
 CREATE SCHEMA IF NOT EXISTS chat;
+
 CREATE TABLE IF NOT EXISTS chat.user (
     userID SERIAL PRIMARY KEY,
     login varchar(30) NOT NULL UNIQUE,
     password varchar(30) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS chat.chatroom (
     chatID SERIAL PRIMARY KEY,
     chatName varchar(30) NOT NULL UNIQUE,
     chatOwner int NOT NULL,
     FOREIGN KEY (chatOwner) REFERENCES chat.user(userID)
 );
+
 CREATE TABLE IF NOT EXISTS chat.message (
     messageID SERIAL PRIMARY KEY,
-    author int REFERENCES chat.user(userID) NOT NULL,
+    author int NOT NULL,
+    FOREIGN KEY (author) REFERENCES chat.user(userID),
     chatRoom int NOT NULL,
     FOREIGN KEY (chatRoom) REFERENCES chat.chatroom(chatID),
-    text text NOT NULL, localDataTime TIMESTAMP NOT NULL
-    );
+    text text NOT NULL,
+    localDataTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS chat.chats(
     chatsID SERIAL PRIMARY KEY,
     userID int NOT NULL,
