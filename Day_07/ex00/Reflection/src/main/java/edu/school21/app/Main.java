ea8c537b6270b8641ebb54800849448b38d29396
Arclight-V <vladimtor@gmail.com>
2021-07-08 11:25:04 +0300

    added dependency for pom

--- Day_07/ex00/Reflection/src/main/java/edu/school21/app/Main.java
+++ Day_07/ex00/Reflection/src/main/java/edu/school21/app/Main.java
@@ -1,7 +1,18 @@
 package edu.school21.app;
 
+import edu.school21.base.Car;
+import edu.school21.base.User;
+import edu.school21.logic.ReflectionChecker;
+
 public class Main {
     public static void main(String[] args) {
+        ReflectionChecker checker = new ReflectionChecker();
+        Car car = new Car();
+        User user = new User();
 
+        checker.showClassName(car);
+        checker.showClassFields(car);
+        checker.showClassName(user);
+        checker.showClassFields(user);
     }
 }
