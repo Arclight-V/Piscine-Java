633f90c88554fee4d52b746e48a87e6e6fceb60a
Arclight-V <vladimtor@gmail.com>
2021-07-08 13:17:54 +0300

    class name

--- Day_07/ex00/Reflection/src/main/java/edu/school21/app/Main.java
+++ Day_07/ex00/Reflection/src/main/java/edu/school21/app/Main.java
@@ -1,8 +1,20 @@
 package edu.school21.app;
 
+import com.google.inject.internal.util.Classes;
 import edu.school21.base.Car;
 import edu.school21.base.User;
 import edu.school21.logic.ReflectionChecker;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +22,27 @@ public class Main {
         Car car = new Car();
         User user = new User();
 
-        checker.showClassName(car);
-        checker.showClassFields(car);
-        checker.showClassName(user);
-        checker.showClassFields(user);
+        checker.showFieldsAnnotations(car);
+
+        System.out.println();
+        checker.showFieldsAnnotations(user);
+
+        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+        classLoadersList.add(ClasspathHelper.staticClassLoader());
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("edu.school21.base"))));
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+
+        for (Class<?> set : classes) {
+
+            System.out.println(set.getName());
+        }
+
+
     }
+
 }
--- Day_07/ex00/Reflection/src/main/java/edu/school21/logic/ReflectionChecker.java
+++ Day_07/ex00/Reflection/src/main/java/edu/school21/logic/ReflectionChecker.java
@@ -1,6 +1,8 @@
 package edu.school21.logic;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class ReflectionChecker {
     public void showClassName(Object object) {
@@ -15,4 +17,24 @@ public class ReflectionChecker {
             System.out.println(field.getName());
         }
     }
+
+    public void showClassMethods(Object object) {
+        Class clazz = object.getClass();
+        Method[] methods = clazz.getMethods();
+        for (Method method: methods) {
+            System.out.println(method.getName());
+        }
+    }
+
+    public void showFieldsAnnotations(Object object) {
+        Class clazz = object.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field: fields) {
+            Annotation[] annotations = field.getAnnotations();
+            for (Annotation annotation : annotations) {
+                System.out.println(annotation.toString());
+            }
+        }
+    }
+
 }
