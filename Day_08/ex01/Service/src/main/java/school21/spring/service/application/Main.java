03756e7abd1c5c9de63f86bf4c71f285041b5ce0
Arclight-V <vladimtor@gmail.com>
2021-07-09 19:53:18 +0300

    AplicationConfig is implemented

--- Day_08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
+++ Day_08/ex01/Service/src/main/java/school21/spring/service/application/Main.java
@@ -2,10 +2,7 @@ package school21.spring.service.application;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import school21.spring.service.models.User;
 import school21.spring.service.repositories.UsersRepository;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
