ff53b570e24c279116fb1b5adda345d30cea3ecb
Arclight-V <vladimtor@gmail.com>
2021-07-09 20:48:37 +0300

    created data for test

---
+++ Day_08/ex02/Service/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product.productTable VALUES
+(default, 'Product1', 'Password1'),
+(default, 'Product2', 'Password2'),
+(default, 'Product3', 'Password3'),
+(default, 'Product4', 'Password4'),
+(default, 'Product5', 'Password5');
