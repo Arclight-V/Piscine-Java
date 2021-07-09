5918a6cc5192134ddfc4f979369d886d3d7fbe74
Arclight-V <vladimtor@gmail.com>
2021-07-09 12:44:17 +0300

    implement findById in UserRepositoryJdbcImp

--- Day_08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepositoryJdbcImpl.java
+++ Day_08/ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepositoryJdbcImpl.java
@@ -3,19 +3,46 @@ package school21.spring.service.repositories;
 import school21.spring.service.models.User;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UsersRepositoryJdbcImpl implements UsersRepository {
 
-    DataSource dataSource;
+    DataSource          dataSource;
+    Connection          connection;
+    PreparedStatement   preparedStatement;
+    ResultSet           resultSet;
+
+    final String selectUserId =  "SELECT * FROM users.user WHERE userID = ";
+
 
     public UsersRepositoryJdbcImpl(DataSource dataSource) {
         this.dataSource = dataSource;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public User findById(Long id) {
+        try {
+            preparedStatement = connection.prepareStatement(selectUserId + id);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User(resultSet.getLong(1), resultSet.getString(2));
+                return user;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
