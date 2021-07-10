154797de839b4a3e585bebfca4d8695021c86e5a
Arclight-V <vladimtor@gmail.com>
2021-07-10 21:28:40 +0300

    "ex01 complited"

--- Day_09/Chat/SocketServer/src/main/java/school21/sockets/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package school21.sockets.repositories;
-
-import school21.sockets.models.User;
-
-import java.util.Optional;
-
-public interface UsersRepository extends CrudRepository<User> {
-    Optional<User> findByEmail(String email);
-}
